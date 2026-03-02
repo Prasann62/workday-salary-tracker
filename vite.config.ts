eeb287958c3e9923f96c04b712226d56d28e4512
prasanna kumar <147803517+Prasann62@users.noreply.github.com>
2026-03-02 22:25:17 +0530

    chore: update netlify url in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+//https://salery-tracker.netlify.app
 export default defineConfig({
   plugins: [react()],
 })
